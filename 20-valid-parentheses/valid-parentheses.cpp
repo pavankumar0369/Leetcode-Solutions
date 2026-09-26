42c6c19cbefe0502005f30e098c3e2fedfbbbba3
pavankumar0369 <pavankumarbkpavankumar@gmail.com>
2026-09-26 15:23:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (66.83%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='[' ||s[i]=='(' ||s[i]=='{'){
+                st.push(s[i]);
+            }else if((!st.empty() && st.top()== '[' && s[i]==']' )||
+                    (!st.empty() && st.top()== '{' && s[i]=='}' )||
+                    (!st.empty() && st.top()== '(' && s[i]==')' )){
+                        st.pop();
+                        continue;
+            }else return false;
+        }
+        return st.size()==0;
+    }
+};
